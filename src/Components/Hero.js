4eec10cef63025ad85a460d2fff2aff37586d402
Mahin <136887053+FaiazurMahin@users.noreply.github.com>
2023-07-05 20:07:35 +0600

    Initial commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import "./Herostyles.css";
+function Hero() {
+  return (
+    <>
+      <div className="Hero">
+        <img
+          alt="HeroImg"
+          src="https://images.unsplash.com/photo-1552083375-1447ce886485?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Hero;
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import Hero from "../Components/Hero";
+import Navbar from "../Components/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
